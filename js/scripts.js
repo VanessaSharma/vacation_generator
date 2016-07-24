78eff2a07121c09065591682801efa33c6cfec5b
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-07-24 10:31:52 -0700

    add more options for conditions

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     var number2 = $("#weather-question").val();
     var number3 = $("#pastime-question").val();
     var result;
-    if (number1 === "Mexican"  && number2 === "Sunny") {
+    if (number1 === "Mexican"||"Jamaican"||"Ethiopian"||"Japanese" && number2 === "Sunny"||"Hot"||"Scorching"||"Warm") {
         document.write("<img src='images/mexico.jpg' />");
-    } else if (number1 === "Indian" && number2 === "Unpredictable") {
+    } else if (number1 === "Indian"||"Thai"||"Peruvian"||"Columbian" && number2 === "Unpredictable"||"Crazy"||"Sunny") {
       document.write("<img src='images/india.jpg' />");
-    } else if (number1 === "Carribean" && number3 === "Exploring nature") {
+    } else if (number1 === "Carribean"||"Jamaica"||"Malaysian" && number3 === "Exploring nature"||"Hiking"||"Nature")
       document.write("<img src='images/carribean.jpg' />");
     }
     
