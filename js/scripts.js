0305b28a31b54d1236d01a21a03272f16322cd7e
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-07-24 18:18:06 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
-  $("form#vacation").submit(function(event) {
+${document).ready(function() {
+  $("form#travel").submit(function(event) {
+    $('form#travel').hide();
     event.preventDefault();
     var number1 = $("#cuisine-question").val();
     var number2 = $("#weather-question").val();
     var number3 = $("#pastime-question").val();
-    var result;
+  
     if (number1 === "Mexican"||"Jamaican"||"Ethiopian"||"Japanese" && number2 === "Sunny"||"Hot"||"Scorching"||"Warm") {
-        document.write("<img src='images/mexico.jpg' />");
-    } else if (number1 === "Indian"||"Thai"||"Peruvian"||"Columbian" && number2 === "Unpredictable"||"Crazy"||"Sunny") {
-      document.write("<img src='images/india.jpg' />");
-    } else if (number1 === "Carribean"||"Jamaica"||"Malaysian" && number3 === "Exploring nature"||"Hiking"||"Nature")
-      document.write("<img src='images/carribean.jpg' />");
+      $('#Mexico').show();
+    } 
+    else if (number1 === "Indian"||"Thai"||"Peruvian"||"Columbian" && number2 === "Unpredictable"||"Crazy"||"Sunny") {
+      $('#India').show();
+    } 
+    else if (number1 === "Carribean"||"Jamaica"||"Malaysian" && number3 === "Exploring nature"||"Hiking"||"Nature") {
+      $('#Carribean').show();
     }
-    
-    $("#output").text(result);
-});
+    else {
+      $('#Sorry').show();
+    }
+  });
+ });
 });
